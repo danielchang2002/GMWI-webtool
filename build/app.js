6b25d2dcc68bfe953ed7144f59da5c81e054341f
danielchang2002 <danielchang2002@gmail.com>
2022-01-12 17:58:36 -0600

    :lipstick: selection box

--- build/app.js
+++ build/app.js
@@ -9,6 +9,8 @@ import { indicies } from "./indicies.js";
 // get element references
 const inputElement = document.getElementById("inputElement");
 const submit = document.getElementById("submit");
+const index_box = document.getElementById("indexBox");
+const pop_box = document.getElementById("popBox");
 const result = document.getElementById("result");
 const histogram = document.getElementById("d3-container");
 
@@ -27,15 +29,18 @@ submit.onclick = (e) => {
       window.alert(message);
       return;
     }
-    // result.innerHTML = get_table(species);
-    const index = Object.keys(indicies)[Math.floor(Math.random() * 5)];
-    const pop = ["healthy", "unhealthy"][Math.floor(Math.random() * 2)];
-    const data = index_data[index][pop];
+    // const index = Object.keys(indicies)[Math.floor(Math.random() * 5)];
+    const index = index_box.value;
+    const pop = pop_box.value;
+    const data =
+      pop === "all"
+        ? index_data[index]["healthy"].concat(index_data[index]["unhealthy"])
+        : index_data[index][pop];
     const score = indicies[index](species);
     const perc = get_percentile(data, score);
     result.innerHTML = `
     ${index} score: ${score} <br/>
-    ${perc}<sup>th</sup> percentile out of ${pop} group:  <br/>
+    ${perc}<sup>th</sup> percentile
     `;
 
     plot_histogram(histogram, score, data);
