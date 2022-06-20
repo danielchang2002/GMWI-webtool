76d85549831b6bb3c5e6aab7ae14b83e45763687
danielchang2002 <danielchang2002@gmail.com>
2022-06-20 11:48:02 -0500

    :lipstick: wording

--- build/app.js
+++ build/app.js
@@ -267,10 +267,6 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
 
 export_button.onclick = () => {
   if (shit_file()) return;
